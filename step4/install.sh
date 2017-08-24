3648625e1437510816d2eb572abe5c54d4a1dfc3
jma78 <jma78@emory.edu>
2017-08-24 15:42:40 -0400

    add dockerfile

---
+++ step4/install.sh
@@ -0,0 +1,13 @@
+set -e
+
+apt-get update
+apt-get install -y software-properties-common
+apt-get install -y octave
+apt-get remove -y software-properties-common
+
+# cleanup package manager
+apt-get autoclean && apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# prepare dir
+mkdir /source
